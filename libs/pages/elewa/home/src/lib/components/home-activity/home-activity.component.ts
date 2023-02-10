6e2277dbd7b1a77d9ef61055a02c2afdc25e5cf2
jeffoty <odhiambo47@gmail.com>
2023-02-10 14:20:22 +0300

    ✨feat: reusable hero feture added

--- libs/pages/elewa/home/src/lib/components/home-activity/home-activity.component.ts
+++ libs/pages/elewa/home/src/lib/components/home-activity/home-activity.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-activity.component.scss'],
 })
 export class HomeActivityComponent {
+  cards = [
+    {
+      subtitle: "Education",
+      imgsrc: "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690300/elewa-group-website/Icons/PNG/Education_hstyzo.png",
+      description: "A key player in the development and operations of new-age human-centered digital learning experiences",
+      title: "Elewa"
+    },
+    {
+      subtitle: 'Technology',
+      imgsrc: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690300/elewa-group-website/Icons/PNG/technolagy_sgfgo6.png',
+      description: 'Offshore tech services which accelerate your business and our talent.',
+      title: 'Italanta'
+    },
+    {
+      subtitle: 'Venture Labs',
+      imgsrc: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690296/elewa-group-website/Icons/SVG/venturelabs_icon_vfnmpj.svg',
+      description: 'Sometimes to grow means to let go.Our bravest mpove into Veenture Labs, where our people they start and <br>accelerate their own startups.',
+      title: 'Italanta'
+    }
+  ]
 
 }
